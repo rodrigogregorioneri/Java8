ad3a4fbdfb5a0c2793c4189f1a6dab9af56a7bf6
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2018-03-27 09:12:22 -0300

    mais exemplos lambda

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,20 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
-        Pessoa user1 = new Pessoa("Priscila Oliveira", 20,1.5f);
-        Pessoa user2 = new Pessoa("Rodrigo Neri", 23,2.3f);
-        Pessoa user3 = new Pessoa("Jose Carlos", 30,1.6f);
+        Pessoa user1 = new Pessoa("Priscila Oliveira", 22,1.5f);
+        Pessoa user2 = new Pessoa("Rodrigo Neri", 24,2.3f);
+        Pessoa user3 = new Pessoa("Jose Carlos", 54,1.6f);
         List<Pessoa> pessoas = Arrays.asList(user1, user2, user3);
         pessoas.forEach(u -> System.out.println(u.getNome()
-                .concat("Idade: \n"+String.valueOf(u.getIdade())
+                .concat("\nIdade: \n"+String.valueOf(u.getIdade())
                 .concat(String.valueOf(" Altura: \n"+u.getAltura())))));
+
+        pessoas.forEach(u -> System.out.println(u.getNome()+", sua data de Nascimento: "+ (GregorianCalendar.getInstance().get(Calendar.YEAR) - u.getIdade()) ));
     }
 }
 
